605fb7b2bb91aa099b1062313982b17adeff2359
den <kdsamp.official@gmail.com>
2022-07-27 19:40:10 +0300

    homework 5, add Contact us, and url site for Contact us

--- app/currency/models.py
+++ app/currency/models.py
@@ -9,5 +9,14 @@ class Rate(models.Model):
     sale = models.DecimalField(max_digits=10, decimal_places=4)  # 999999.9999
     buy = models.DecimalField(max_digits=10, decimal_places=4)
     source = models.CharField(max_length=64)
-    created = models.DateTimeField(auto_created=True)
+    created = models.DateTimeField(auto_now_add=True)
 
+
+class ContactUs(models.Model):
+
+    mail_id = models.DecimalField(max_digits=10, decimal_places=4)
+    email_from = models.EmailField(max_length=64)
+    email_to = models.EmailField(max_length=64)
+    subject = models.CharField(max_length=32)
+    message = models.TextField(max_length=999)
+    date_message = models.DateTimeField(auto_now_add=True)
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -2,10 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from currency.views import generate_password, hello_world
+from currency.views import generate_password, hello_world, contact_us, rate_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('generate-password/', generate_password),
     path('hello/', hello_world),
+    path('contact-us/', contact_us),
+    path('rate/list/', rate_list)
 ]
