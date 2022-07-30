6bac71dffd6fe89b9aa59dff6da223ec8f16e59f
den <kdsamp.official@gmail.com>
2022-07-30 22:32:50 +0300

    Homework 6. Create contact_us.html table update rate_list table

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,41 +1,25 @@
 
 from django.shortcuts import render
-from django.http import HttpResponse
-from currency.utils import get_password, get_rate_list, get_contact_us
-
-# Create your views here.
+from currency.models import Rate, ContactUs
 
 
-def generate_password(request):
-    length = int(request.GET.get('length'))
-
-    password = get_password(length)
-    return HttpResponse(password)
-
-
-def hello_world(request):
-    return HttpResponse('Hello world!')
+def index_page(request):
+    return render(request, 'index.html')
 
 
 def rate_list(request):
 
-    # cont_us = get_contact_us()
-    # contact_us_html = []
-    # for c in cont_us:
-    #     contact_us_html.append(f'<br>{c}</br>')
-
     context = {
-        'message': 'HELLO TEXT'
-        }
+        'rate_list': Rate.objects.all(),
+    }
 
     return render(request, 'rate_list.html', context=context)
 
 
 def contact_us(request):
 
-    cont_us = get_contact_us()
-    contact_us_html = []
-    for c in cont_us:
-        contact_us_html.append(f'<br>{c}</br>')
+    context = {
+        'contact_us': ContactUs.objects.all(),
+    }
 
-    return HttpResponse(str(contact_us_html))
+    return render(request, 'contact_us.html', context=context)
