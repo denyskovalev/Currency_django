3f7305132d2c89edd18a1b46dce27858ced961b3
den <kdsamp.official@gmail.com>
2022-08-01 12:52:17 +0300

    create urls for rate list

--- app/currency/views.py
+++ app/currency/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render
 from currency.models import Rate, ContactUs
 
 
+# Index functions
 def index_page(request):
     return render(request, 'index.html')
 
 
+# Rate list functions
 def rate_list(request):
 
     context = {
@@ -16,6 +18,16 @@ def rate_list(request):
     return render(request, 'rate_list.html', context=context)
 
 
+def create_rate_list(request):
+
+    context = {
+
+    }
+
+    return render(request, 'create_rate_list.html', context=context)
+
+
+# Contact us functions
 def contact_us(request):
 
     context = {
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -2,11 +2,16 @@
 from django.contrib import admin
 from django.urls import path
 
-from currency.views import contact_us, rate_list, index_page
+from currency.views import contact_us, rate_list, index_page, create_rate_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('contact-us/', contact_us),
-    path('rate/list/', rate_list),
     path('', index_page),
+
+    # Rate list
+    path('rate/list/', rate_list),
+    path('rate/create/', create_rate_list),
+
+    # Contact us
+    path('contact-us/', contact_us),
 ]
