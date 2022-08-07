f575575bf0e41f36b5d9766ba5e8d78d04bc683d
den <kdsamp.official@gmail.com>
2022-08-07 21:52:02 +0300

    add 'currency/' to path/include (settings/urls.py) homework #8 done

--- app/settings/urls.py
+++ app/settings/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.IndexView.as_view()),
 
-    path('', include('currency.currency_urls')),
+    path('currency/', include('currency.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
